b83e9dca8868d65527eaae2c69caf920c91a3f7d
Yibo Lin <yibolin@utexas.edu>
2015-05-30 23:39:57 -0500

    new makefile framework for gdsii stream parser; plan to extend to other components

---
+++ Darwin.mk
@@ -0,0 +1,19 @@
+#==========================================================================
+#                    Configuration under darwin environment
+# ==========================================================================
+
+# detect compiler 
+ifneq ($(shell which clang++),)
+	CXX = clang++
+	AR = ar
+else
+	CXX = g++
+	AR = ar
+endif
+
+CXXFLAGS_BASIC = -ferror-limit=1 -W -Wall -Wextra -Wreturn-type -m64 -Wno-deprecated -stdlib=libstdc++
+CXXFLAGS_DEBUG = -g -DDEBUG $(CXXFLAGS_BASIC) 
+CXXFLAGS_RELEASE = -O4 $(CXXFLAGS_BASIC) 
+
+ARFLAGS = rvs
+
---
+++ Unix.mk
@@ -0,0 +1,23 @@
+#==========================================================================
+#                    Configuration under unix environment
+# ==========================================================================
+
+# detect compiler 
+ifneq ($(shell which g++48),)
+	CXX = g++48
+	AR = ar
+else
+ifneq ($(shell which g++47),)
+	CXX = g++47
+	AR = ar
+else 
+	CXX = g++
+	AR = ar
+endif
+endif
+
+CXXFLAGS_BASIC = -fmax-errors=1 -W -Wall -Wextra -Wreturn-type -ansi -m64 -Wno-deprecated
+CXXFLAGS_DEBUG = -g -DDEBUG $(CXXFLAGS_BASIC) 
+CXXFLAGS_RELEASE = -O4 -fopenmp $(CXXFLAGS_BASIC) 
+
+ARFLAGS = rvs
