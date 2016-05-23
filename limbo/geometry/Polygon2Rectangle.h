1b5bb8766403014b2323d8568d19e997016bc320
Yibo Lin <yibolin@utexas.edu>
2016-05-22 22:00:20 -0500

    changed api of polygon2rectangle for efficiency

--- limbo/geometry/Polygon2Rectangle.h
+++ limbo/geometry/Polygon2Rectangle.h
@@ -91,14 +91,16 @@ class Polygon2Rectangle
 		/// if slicing_orient == HORIZONTAL_SLICING or slicing_orient == VERTICAL_SLICING, only 1 copy of points is stored 
 		/// if slicing_orient == HOR_VER_SLICING, 2 copies of points sorted by different orientation are stored 
 		/// btw, slicing orientation is perpendicular to its corresponding sorting orientation 
-		Polygon2Rectangle(slicing_orientation_2d slicing_orient = HORIZONTAL_SLICING)
+		Polygon2Rectangle(rectangle_set_type& vRect, slicing_orientation_2d slicing_orient = HORIZONTAL_SLICING)
+            : m_vRect(vRect)
 		{
 			this->initialize(slicing_orient);
 		}
 		/// constructor 
 		/// \param InputIterator represents the iterator type of point set container for construction only 
 		template <typename InputIterator>
-		Polygon2Rectangle(InputIterator input_begin, InputIterator input_end, slicing_orientation_2d slicing_orient = HORIZONTAL_SLICING)
+		Polygon2Rectangle(rectangle_set_type& vRect, InputIterator input_begin, InputIterator input_end, slicing_orientation_2d slicing_orient = HORIZONTAL_SLICING)
+            : m_vRect(vRect)
 		{
 			this->initialize(slicing_orient);
 			this->initialize(input_begin, input_end);
@@ -289,11 +291,6 @@ class Polygon2Rectangle
 		{
 			return m_vRect;
 		}
-        /// faster api, but user must take care of data race 
-        void get_rectangles(rectangle_set_type& vRect)
-        {
-            m_vRect.swap(vRect);
-        }
 		/// read polygon from file 
 		/// try to be compatible to gnuplot format 
 		bool read(string const& filename)
@@ -541,7 +538,7 @@ class Polygon2Rectangle
 													///< 1~2 copies, sort by left lower or by lower left 
 													///< for HORIZONTAL key, sort by left lower 
 													///< for VERTICAL key, sort by lower left 
-		rectangle_set_type m_vRect; ///< save all rectangles from conversion 
+		rectangle_set_type& m_vRect; ///< save all rectangles from conversion 
 };
 
 /// \brief standby function for polygon-to-rectangle conversion 
@@ -552,9 +549,8 @@ template <typename InputIterator, typename PointSet, typename RectSet>
 inline bool polygon2rectangle(InputIterator input_begin, InputIterator input_end, 
 		PointSet const&, RectSet& r, slicing_orientation_2d slicing_orient = HORIZONTAL_SLICING)
 {
-	Polygon2Rectangle<PointSet, RectSet> p2r (input_begin, input_end, slicing_orient);
+	Polygon2Rectangle<PointSet, RectSet> p2r (r, input_begin, input_end, slicing_orient);
 	if (!p2r()) return false;
-	p2r.get_rectangles(r);
 	return true;
 }
 
