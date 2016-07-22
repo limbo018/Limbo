2ee3167568d9029a4413ad7e6134f03509fbe902
Yibo Lin <yibolin@utexas.edu>
2016-07-22 19:51:21 +0800

    fix the dependency of parsers to flex 2.5.37; now it supports most versions including 2.5.35, 2.5.37, 2.6.0

--- Darwin.mk
+++ Darwin.mk
@@ -21,3 +21,15 @@ ARFLAGS = rvs
 
 LEX = flex
 YACC = bison
+
+# detect flex version for FlexLexer.h
+# a backup for version 2.5.37 is available in the library to be compatible with previous releases of limbo 
+# for other versions, user need to specify FLEX_DIR environment variable 
+ifneq ($(findstring 2.5.37,$(shell $(LEX) --version)),)
+	LEX_INCLUDE_DIR = $(LIMBO_ROOT_DIR)/limbo/thirdparty/flex/2.5.37
+else 
+# LEX_INCLUDE_DIR is not set as environment variable, so that user has more flexibility to set either FLEX_DIR or LEX_INCLUDE_DIR 
+ifeq ($(LEX_INCLUDE_DIR),) 
+	LEX_INCLUDE_DIR = $(FLEX_DIR)/include # the path to FlexLexer.h 
+endif 
+endif 
