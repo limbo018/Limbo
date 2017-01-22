56826c22b6face26d5e1cc1cb5f6c7243667b0f6
Yibo Lin <yibolin@utexas.edu>
2017-01-21 23:28:48 -0600

    fix linker bug with rpath= to rpath,

--- limbo/makeutils/FindBoost.mk
+++ limbo/makeutils/FindBoost.mk
@@ -9,7 +9,7 @@
 BOOST_LINK_FLAG :=
 # use libboost_system.a to test whether boost has static library
 ifeq ($(wildcard $(BOOST_DIR)/lib/libboost_system.a),) 
-	BOOST_LINK_FLAG = -Wl,-rpath=$(BOOST_DIR)/lib $(DYNAMIC_LINK_FLAG)
+	BOOST_LINK_FLAG = -Wl,-rpath,$(BOOST_DIR)/lib $(DYNAMIC_LINK_FLAG)
 else 
 	BOOST_LINK_FLAG = $(STATIC_LINK_FLAG)
 endif 
--- limbo/makeutils/FindZlib.mk
+++ limbo/makeutils/FindZlib.mk
@@ -9,7 +9,7 @@
 ZLIB_LINK_FLAG =
 # use libz.a to test whether Zlib has static library
 ifeq ($(wildcard $(ZLIB_DIR)/libz.a),) 
-	ZLIB_LINK_FLAG = -Wl,-rpath=$(ZLIB_DIR) $(DYNAMIC_LINK_FLAG)
+	ZLIB_LINK_FLAG = -Wl,-rpath,$(ZLIB_DIR) $(DYNAMIC_LINK_FLAG)
 else 
 	ZLIB_LINK_FLAG = $(STATIC_LINK_FLAG)
 endif 
