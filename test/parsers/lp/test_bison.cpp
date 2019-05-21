f8af32063e656cf39ab43d79881d8a3b7eb9468d
Yibo Lin <yibolin@utexas.edu>
2019-05-21 15:19:09 -0500

    add cmake support

--- test/parsers/lp/test_bison.cpp
+++ test/parsers/lp/test_bison.cpp
@@ -39,8 +39,9 @@ class LpDataBase : public LpParser::LpDataBase
         /// @param terms array of terms in left hand side 
         /// @param compare operator '<', '>', '='
         /// @param constant constant in the right hand side 
-		void add_constraint(LpParser::TermArray const& terms, char compare, double constant) 
+		void add_constraint(string const& cname, LpParser::TermArray const& terms, char compare, double constant) 
 		{
+            cout << cname << ": ";
             for (LpParser::TermArray::const_iterator it = terms.begin(); it != terms.end(); ++it)
                 cout << " + " << it->coef << " " << it->var; 
             cout << " " << compare << " " << constant << endl; 
